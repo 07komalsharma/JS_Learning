d2b94e12ca80dc2ede3f87be04e808db9c79f0c1
Komal Sharma <56592343+07komalsharma@users.noreply.github.com>
2023-10-12 12:22:48 +0530

    Add files via upload

---
+++ 01_basics/MathFunction.js
@@ -0,0 +1,24 @@
+//
+// const nm=new Number(200)
+// console.log(nm.length());
+ 
+// let v=nm.isInteger
+// console.log(v);
+
+//++++++++++ Maths.........
+
+console.log(Math)
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
+console.log(Math.max(4,7,9,3));
+console.log(Math.min(1,2,3,0));
+console.log((Math.random()*10)+1);//for min value 1 
+
+//wants random no. in between 10 & 20
+const max=20
+const min=10
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
